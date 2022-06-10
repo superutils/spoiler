dd5383beac5bdff51f1c3b3c2d7bc262093d189b
Daniil Orain <dnlorn@gmail.com>
2022-06-10 11:25:08 +1000

    try new version

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import Spoiler from './components/Spoiler'
+// import * as ReactDOM from 'react-dom';
+export default Spoiler
+
+// const element = (
+//   <>
+//     <Spoiler>
+//       <h1>Hello!</h1>
+//   </Spoiler>
+//   </>
+// );
+//
+// ReactDOM.render(element, document.getElementById('root'));
