076cfdcbb9b25a4bd26ac79b2dd5e91e15ad5db6
Daniil Orain <dnlorn@gmail.com>
2022-06-10 15:21:35 +1000

    start from scratch

--- lib/cjs/components/Spoiler.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface ISpoiler {
-    children?: JSX.Element | JSX.Element[];
-    width?: string | number;
-    height?: string | number;
-}
-export default function Spoiler(props: ISpoiler): JSX.Element;
-export {};
--- src/index.tsx
+++
@@ -1,2 +0,0 @@
-import Spoiler from './components/Spoiler'
-export default Spoiler
